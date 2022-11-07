b87715ad0236f8f1bfdd01936cd59dbc04c70d18
Cheng Zhou <chzh3458@colorado.edu>
2022-11-07 11:12:34 -0700

    Made basic changes to tables

--- init_data/create.sql
+++ init_data/create.sql
@@ -2,26 +2,29 @@ DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
     user_id SERIAL primary key,
     username varchar(50) not null,
-    password char(50) not null,
-    email char(50) not null,
-    name char(50) not null
+    password varchar(50) not null,
+    email varchar(50) not null,
+    name varchar(50) not null,
+    phone varchar(11) not null
 );
 
 CREATE TABLE tickets (
     ticket_id SERIAL primary key,
-    price int not null,
-    event_type char(50),
-    location char(50),
-    time real,
-    date date
+    price real not null,
+    event_type varchar(50),
+    location varchar(50),
+    date date,
+    time time
 );
 
 CREATE TABLE reviews (
     review_id SERIAL primary key,
-    rating REAL,
+    date date,
+    rating real,
     review varchar(500)
 );
 
+-- Seller to tickets
 CREATE TABLE users_to_tickets (
     user_id not null,
     ticket_id not null,
@@ -29,6 +32,15 @@ CREATE TABLE users_to_tickets (
     FOREIGN KEY (ticket_id) REFERENCES tickets(ticket_id)
 );
 
+-- Buyer to tickets
+CREATE TABLE tickets_to_users (
+    user_id not null,
+    ticket_id not null,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (ticket_id) REFERENCES tickets(ticket_id)
+)
+
+-- Shows reviews on user, not reviewer!!
 CREATE TABLE users_to_reviews (
     user_id not null,
     review_id not null,
