57cd61554d8452ca83209e3e3b120f9392616a4c
Louis Marfone <loma4620@colorado.edu>
2022-11-11 12:41:05 -0700

    index page updates

--- src/index.js
+++ src/index.js
@@ -5,32 +5,6 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
 const axios = require('axios');
-<<<<<<< HEAD
-=======
-
-
-
-// database configuration
-const dbConfig = {
-  host: 'db',
-  port: 5432,
-  database: process.env.POSTGRES_DB,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-};
-
-const db = pgp(dbConfig);
-
-// test your database
-db.connect()
-  .then(obj => {
-      console.log('Database connection successful'); // you can view this message in the docker compose logs
-      obj.done(); // success, release the connection;
-  })
-  .catch(error => {
-      console.log('ERROR:', error.message || error);
-});
->>>>>>> 4750a15ef28a829201cc039e5ddd1b0fd698a7ed
 
 // database configuration
 const dbConfig = {
@@ -77,7 +51,6 @@ app.use(
 app.listen(3000);
 console.log('Server is listening on port 3000');
 
-<<<<<<< HEAD
 app.get('/', (req, res) => {
     res.render('pages/home');
 });
@@ -150,18 +123,4 @@ app.get('/search_results', (req, res) => {
             message: err.message,
         });
       });
-=======
-const tickets_db = () => {
-  db.any(query).then(data => {
-    return data;
-  });
-}
-
-app.get('/', (req, res) =>{
-  res.redirect('/home');
-});
-
-app.get('/home', (req, res) =>{
-  res.render('pages/home.ejs');
->>>>>>> 4750a15ef28a829201cc039e5ddd1b0fd698a7ed
 });
