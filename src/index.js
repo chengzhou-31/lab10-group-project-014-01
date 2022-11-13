04df0a98bb625fbca28396636a5ab9658e46d72f
Diego Marrero <dima5248@colorado.edu>
2022-11-13 14:55:59 -0700

    Adjusted Login-Page visual presentation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ const app = express();
 const pgp = require('pg-promise')();
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const bcrypt = require('bcrypt');
+const axios = require('axios');
+
+
+//Database config - not sure if required:
+const dbConfig = {
+  host: 'db',
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+};
+
+const db = pgp(dbConfig);
+
 
 app.set('view engine', 'ejs');
 
@@ -33,11 +48,11 @@ app.get('/test', (req, res) =>{
   res.render('pages/test.ejs');
 });
 
+
+
 app.get('/login', (req,res) => {
   res.render('pages/login');
 });
-
-
 
 app.post('/login', async (req, res) => {
   const query = "SELECT * FROM users WHERE username = $1;"
